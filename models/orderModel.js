8faa461e17dbcac4d4b49644c3cc6fddff596cdb
Chinmoy Chakraborty <chinmoychakraborty009@gmail.com>
2021-07-04 18:25:20 +0600

    Implement getOrderDetails end point

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
     orderItems: [{
@@ -86,4 +86,4 @@ const orderSchema = new mongoose.Schema({
 });
 
 const Order = mongoose.model('Order', orderSchema);
-export default Order;
+module.exports = Order;
